01a0c9a71655fefcf036100bf93a1aa316d7c098
Rounak <rounakdatta12@gmail.com>
2020-07-14 23:40:22 +0530

    day_1 add more tests

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	go test ./...
+
+test-verbose:
+	go test -v ./...
+
+clean:
+	go clean -testcache
--- one/one_test.go
+++ one/one_test.go
@@ -13,3 +13,13 @@ func Test1(t *testing.T) {
 		t.Errorf("Got %T, expected %T", result, true)
 	}
 }
+
+func Test2(t *testing.T) {
+	numbers := []int{1, 6, 3, 7, 5}
+	k := 8
+
+	result, err := Run(numbers, k)
+	if err != nil {
+		t.Errorf("Got %T, expected %T", result, false)
+	}
+}
